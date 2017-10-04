dda44ef335203d750ec7e7be7d66305c0db180d2
tijeco <coleti16@students.ecu.edu>
2017-10-04 06:40:22 -0400

    csv as input pdf as output

--- csv2tsne.R
+++ csv2tsne.R
@@ -1,6 +1,11 @@
-## calling the installed package
-train<- read.csv(file.choose()) ## Choose the train.csv file downloaded from the link above  
 library(Rtsne)
+args <- commandArgs(TRUE)
+
+## calling the installed package
+inFile <- args[1]
+outFile <- args[2]
+train<- read.csv(inFile) ## Choose the train.csv file downloaded from the link above  
+
 ## Curating the database for analysis with both t-SNE and PCA
 Labels<-train$label
 train$label<-as.factor(train$label)
@@ -13,4 +18,8 @@ tsne <- Rtsne(train[,-1], dims = 2, perplexity=30, verbose=TRUE, max_iter = 500)
 #exeTimeTsne<- system.time(Rtsne(train[,-1], dims = 2, perplexity=30, verbose=TRUE, max_iter = 500))
 
 ## Plotting
+pdf(outFile)
 plot(tsne$Y, main="tsne")
+text(tsne$Y, labels=train$label, col=colors[train$label])
+dev.off()
+
